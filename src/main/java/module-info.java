a3cec8978a2dc002b5dc423b4fc2465a350e1a06
UsernameLuxi <christensendalbylucas@gmail.com>
2025-01-07 10:20:22 +0100

    lib filer + db connector

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dk.easv.mytunes.privatemoviecollection {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
 
     opens dk.easv.mytunes.privatemoviecollection to javafx.fxml;
